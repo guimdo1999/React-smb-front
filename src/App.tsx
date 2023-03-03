430c0a0de5a20a5b044942bf5a444c4040cc3c66
Guilherme Oliveira <guimdo1999@hotmail.com>
2023-03-03 09:30:03 -0300

    Removendo dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "fomantic-ui-css/semantic.css";
-import { BrowserRouter , Switch, Route, withRouter } from "react-router-dom";
+import { BrowserRouter , Switch, Route } from "react-router-dom";
 import "./App.css";
 import CadastrarEditar from "./pages/Cadastrar-editar";
 import Editar from "./pages/Editar";
@@ -10,9 +10,15 @@ function App() {
     <>
     <BrowserRouter>
       <Switch>
-        <Route exact path="/build" component={withRouter(ListarCadastros)} />
-        <Route exact path="/build/cadastrar" component={withRouter(CadastrarEditar)} />
-        <Route exact path="/build/editar/:id" component={withRouter(Editar)} />
+          <Route exact path="/build">
+            <ListarCadastros/>
+        </Route>
+          <Route exact path="/build/cadastrar"  >
+            <CadastrarEditar/>
+        </Route>
+          <Route exact path="/build/editar/:id"  >
+            <Editar/>
+        </Route>
       </Switch>
     </BrowserRouter>
     </>
